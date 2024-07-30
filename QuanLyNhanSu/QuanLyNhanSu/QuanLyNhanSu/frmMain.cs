1718b515385bc049d0d5b8e3d84b3784c9ec9044
nqanh12 <134897944+nqanh12@users.noreply.github.com>
2024-07-30 23:05:31 +0700

    QuocAnh
    update software

---
+++ QuanLyNhanSu/QuanLyNhanSu/QuanLyNhanSu/frmMain.cs
@@ -0,0 +1,50 @@
+﻿using MetroFramework.Controls;
+using MetroFramework.Forms;
+using QuanLyNhanSu.DangNhap;
+using System.Windows.Forms;
+
+namespace QuanLyNhanSu
+{
+    public partial class frmMain : MetroForm
+    {
+        private frmMain()
+        {
+            InitializeComponent();
+        }
+
+        #region Add by Me
+        private static frmMain frm;
+
+        public static frmMain getFrmMain
+        {
+            get
+            {
+                if (frm == null)
+                {
+                    frm = new frmMain();
+                }
+                return frm;
+            }
+        }
+
+        public MetroPanel getMetroPanelContainer
+        {
+            get
+            {
+                return mpnContainer;
+            }
+        }
+        #endregion
+
+        private void frmMain_Load(object sender, System.EventArgs e)
+        {
+            //_frmMain = this;
+            ucDangNhap ucDN = new ucDangNhap();
+            ucDN.Dock = DockStyle.Fill;
+            //FrmMain.MetroPanelContainer.Controls.Add(ucDN);
+            //FrmMain.MetroPanelContainer.Controls["ucDangNhap"].BringToFront();
+            getFrmMain.getMetroPanelContainer.Controls.Add(ucDN);
+            getFrmMain.getMetroPanelContainer.Controls["ucDangNhap"].BringToFront();
+        }
+    }
+}
