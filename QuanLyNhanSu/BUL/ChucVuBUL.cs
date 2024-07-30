dfe04fd0c04ea5b380465f19fbe525c1fd4a7c9b
nqanh12 <134897944+nqanh12@users.noreply.github.com>
2024-07-30 23:12:19 +0700

    QuocAnh
    update project

---
+++ QuanLyNhanSu/BUL/ChucVuBUL.cs
@@ -0,0 +1,35 @@
+﻿using DAL.DAL;
+using DTO;
+using EF;
+using System.Collections.Generic;
+
+namespace BUL
+{
+    public class ChucVuBUL
+    {
+        public static List<CHUCVU> LoadChucVu()
+        {
+            return ChucVuDAL.LoadChucVu();
+        }
+
+        public static List<ChucVuDTO> LoadSoLuongNhanVienTungChucVu()
+        {
+            return ChucVuDAL.LoadSoLuongNhanVienTungChucVu();
+        }
+
+        public static ChucVuDTO ThemChucVu(ChucVuDTO chucVuDTO)
+        {
+            return ChucVuDAL.ThemChucVu(chucVuDTO);
+        }
+
+        public static ChucVuDTO SuaChucVu(ChucVuDTO chucVuDTO)
+        {
+            return ChucVuDAL.SuaChucVu(chucVuDTO);
+        }
+
+        public static ChucVuDTO XoaChucVu(ChucVuDTO chucVuDTO)
+        {
+            return ChucVuDAL.XoaChucVu(chucVuDTO);
+        }
+    }
+}
